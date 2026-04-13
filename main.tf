ef42c8113d7de3522b044512b19005f778a2d551
kapil deo <kapildeo@kapils-MacBook-Air.local>
2026-04-13 12:59:05 +0530

    stg add

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-terraform"
-    location = "eastus"
+  name     = "rgterraform"
+  location = "centralindia"
+
+}
+resource "azurerm_storage_account" "stg2" {
+    name                     = "stgterraform2"
+    resource_group_name      = azurerm_resource_group.rg.name
+    location                 = azurerm_resource_group.rg.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
   
 }
+
